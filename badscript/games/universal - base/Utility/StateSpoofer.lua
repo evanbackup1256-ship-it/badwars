57a815c88205f1edc3ef83528aee2297f3d0adcb
evanbackup1256-ship-it <evanbackup1256@gmail.com>
2026-07-03 22:37:15 -0400

    fix: resolve nil indexing crashes in StateSpoofer.lua Utility module
    - Add nil guards for rakNetCheck access
    - Add nil guards for packet.AsArray and packet.AsBuffer access
    - Add nil guards for State.Value access
    - Add nil guards for raknet.add_send_hook and raknet.remove_send_hook access

--- badscript/games/universal - base/Utility/StateSpoofer.lua
+++ badscript/games/universal - base/Utility/StateSpoofer.lua
@@ -6,22 +6,28 @@ StateSpoofer = Bad.Categories.Utility:CreateModule({
 	Name = 'StateSpoofer',
 	Function = function(callback)
 		if callback then
-			if not rakNetCheck('StateSpoofer') then
+			if not rakNetCheck or not rakNetCheck('StateSpoofer') then
 				StateSpoofer:Toggle()
 				return
 			end
 
 			hook = function(packet)
-				if packet.AsArray[1] == 0x1b then
+				if packet and packet.AsArray and packet.AsArray[1] == 0x1b and packet.AsBuffer then
 					local data = packet.AsBuffer
-					buffer.writeu8(data, 25, Enum.HumanoidStateType[State.Value].Value + 32)
-					packet:SetData(data)
+					if State and State.Value and Enum.HumanoidStateType[State.Value] then
+						buffer.writeu8(data, 25, Enum.HumanoidStateType[State.Value].Value + 32)
+						packet:SetData(data)
+					end
 				end
 			end
 
-			raknet.add_send_hook(hook)
+			if raknet and raknet.add_send_hook then
+				raknet.add_send_hook(hook)
+			end
 		elseif hook then
-			raknet.remove_send_hook(hook)
+			if raknet and raknet.remove_send_hook then
+				raknet.remove_send_hook(hook)
+			end
 			hook = nil
 		end
 	end,

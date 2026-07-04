4e17f209b6a94d71365fc3ed4a823bf20ba58a15
evanbackup1256-ship-it <evanbackup1256@gmail.com>
2026-07-03 22:32:01 -0400

    fix: resolve nil indexing crashes in Rejoin.lua Utility module
    - Add nil guards for teleportService access
    - Add nil guards for playersService.NumPlayers access

--- badscript/games/universal - base/Utility/Rejoin.lua
+++ badscript/games/universal - base/Utility/Rejoin.lua
@@ -7,10 +7,12 @@ Rejoin = Bad.Categories.Utility:CreateModule({
 			notif('Rejoin', 'Rejoining...', 5)
 			Rejoin:Toggle()
 
-			if playersService.NumPlayers > 1 then
-				teleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
-			else
-				teleportService:Teleport(game.PlaceId)
+			if teleportService then
+				if playersService and playersService.NumPlayers > 1 then
+					teleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+				else
+					teleportService:Teleport(game.PlaceId)
+				end
 			end
 		end
 	end,
